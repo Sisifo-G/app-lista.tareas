57c0382ac5dc465ee6f4a126bd8a4cf44fd10133
Sisifo-G <manuelperdomo1710@icloud.com>
2021-01-30 13:39:38 -0500

    Agregada funcionalidad a botón editar tarea

--- src/components/Tarea.js
+++ src/components/Tarea.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckSquare, faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 
 const Tarea = ({tarea}) => {
+
+    const [ editandoTarea, setEditandoTarea ] = useState(false);
+    const [ nuevaTarea, setNuevaTarea ] = useState(tarea.texto);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setEditandoTarea(false);
+    }
+
     return ( 
         <li className="lista-tareas__tarea"> 
             <FontAwesomeIcon icon={faCheckSquare} className="lista-tareas__icono lista-tareas__icono-check" />
             <div className="lista-tareas___texto">
-                {tarea.texto}
+                {editandoTarea ? 
+                    <form className="formulario-editar-tarea" onSubmit={handleSubmit}>
+                        <input 
+                            type="text" 
+                            className="formulario-editar-tarea__input"
+                            value={nuevaTarea}
+                            onChange={(e) => setNuevaTarea(e.target.value)}
+                        />
+                        <button type="submit" className="formulario-editar-tarea__btn">Actualizar</button>
+                    </form>
+                    : tarea.texto
+                }
             </div> 
             <div className="lista-tareas__contenedor-botones">
-            <FontAwesomeIcon icon={faEdit} className="lista-tareas__icono lista-tareas__icono-accion" />
+            <FontAwesomeIcon icon={faEdit} className="lista-tareas__icono lista-tareas__icono-accion" onClick={() => setEditandoTarea(!editandoTarea)}/>
             <FontAwesomeIcon icon={faTimes} className="lista-tareas__icono lista-tareas__icono-accion" />
             </div>
 
         </li>
-     );
+    );
 }
- 
+
 export default Tarea;
