c47c0693ff9aa39c85278da20a754b5b61d1ea74
Sisifo-G <manuelperdomo1710@icloud.com>
2021-01-30 11:16:46 -0500

    Estilos de lista de tareas agregado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import FormularioTareas from './components/FormularioTareas'
+import ListaTareas from './components/ListaTareas';
 const App = () => {
 
   const [ tareas, setTareas ] = useState(
@@ -23,6 +24,7 @@ const App = () => {
     <div className="contenedor">
      <Header />
      <FormularioTareas tareas={tareas} setTareas={setTareas} />
+     <ListaTareas tareas={tareas}/>
     </div>
   );
 }
